8f5f88bfe4b62e2053b0f1fda348f051983ca33c
conconrob <connortprobertson@gmail.com>
2019-04-12 08:29:38 +0100

    first test

---
+++ server.test.js
@@ -0,0 +1,19 @@
+const server = require("./server");
+const request = require("supertest");
+
+const testServer = request(server);
+//  {
+//     title: 'Pacman', // required
+//     genre: 'Arcade', // required
+//     releaseYear: 1980 // not required
+//   }
+describe("server", () => {
+  describe("POST /games endpoint", () => {
+    it("returns a 422 when the body does not have all required data", () => {
+      return testServer
+        .post("/games")
+        .send({ title: "Megaman", releaseYear: 1990 })
+        .expect(422);
+    });
+  });
+});
