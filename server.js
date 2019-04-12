92f71a80774491dc3bfe1f884ce0c8da019ed54a
conconrob <connortprobertson@gmail.com>
2019-04-12 08:32:32 +0100

    made first test pass

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+server.post("/games", (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+  if (!title || !genre) {
+    res.sendStatus(422);
+  }
+});
+
 module.exports = server;
