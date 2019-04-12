4bfb21168db6262fb4962dd2fbbf9428ab45f4a2
conconrob <connortprobertson@gmail.com>
2019-04-12 08:41:27 +0100

    made test pass

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+const games = [];
+const id = 1;
 server.post("/games", (req, res) => {
   const { title, genre, releaseYear } = req.body;
   if (!title || !genre) {
     res.sendStatus(422);
+  } else {
+    games.push({ title, genre, releaseYear, id });
+    res.status(201).json(games.find(game => game.id === id));
+    ++id;
   }
 });
 
